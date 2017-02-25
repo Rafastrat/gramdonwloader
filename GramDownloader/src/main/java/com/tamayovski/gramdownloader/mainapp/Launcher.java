a25f852869def882bdc4b36b9e3d6c18cb3eb4fd
Skynet\Rafa <rtamayoasensio@gmail.com>
2017-02-25 18:58:46 +0100

    Se obtiene fuentes y metas

--- GramDownloader/src/main/java/com/tamayovski/gramdownloader/mainapp/Launcher.java
+++ GramDownloader/src/main/java/com/tamayovski/gramdownloader/mainapp/Launcher.java
@@ -1,10 +1,19 @@
 package com.tamayovski.gramdownloader.mainapp;
 
+import java.io.IOException;
+
+import com.tamayovski.gramdownloader.utils.Parser;
+
 public class Launcher {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Parser parser = new Parser();
+		try {
+			parser.getHighQualityImageUrl(parser.getPrimaryImageUrl("C:/Users/Rafa/Desktop/new 3.html"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
--- GramDownloader/src/main/java/com/tamayovski/gramdownloader/utils/Parser.java
+++ GramDownloader/src/main/java/com/tamayovski/gramdownloader/utils/Parser.java
@@ -1,8 +1,93 @@
 package com.tamayovski.gramdownloader.utils;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.commons.io.IOUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class Parser {
 
-	public void getImagesUrl(){
+	private static final String BASE_URL = "https://www.instagram.com";
+
+	public List<String> getPrimaryImageUrl(String input) throws IOException {
+		System.out.println("Extrayendo urls de publicaciones...");
+		List<String> imageUrl = new LinkedList<>();
+		File in = new File(input);
+		Document doc = Jsoup.parse(in, null);
+		Elements pngs = doc.select("a");
+		System.out.println("Extraidas " + pngs.size() + " urls.");
+		System.out.println("Mostrando urls: ");
+		for (Iterator iterator = pngs.iterator(); iterator.hasNext();) {
+			Element element = (Element) iterator.next();
+			String url = element.attr("href");
+			imageUrl.add(BASE_URL + url);
+			System.out.println(BASE_URL + url);
+		}
+		return imageUrl;
+	}
+	public List<String> getHighQualityImageUrl(List<String> publicaciones) throws IOException {
+		System.out.println("Extrayendo urls para imagenes de alta calidad...");
+		System.out.println("Mostrando urls: ");
+		List<String> imageUrl = new LinkedList<>();
 		
+		for (String url : publicaciones) {
+			/*Document doc = Jsoup.connect(url).followRedirects(true).ignoreContentType(true).timeout(12000) // optional
+					.header("Accept-Language", "pt-BR,pt;q=0.8") // missing
+					.header("Accept-Encoding", "gzip,deflate,sdch") // missing
+					.userAgent(
+							"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.107 Safari/537.36") // missing
+					.referrer("http://www.google.com") // optional
+					.execute().parse();*/
+			/*String docu = "";
+			Observable.from(ClientBuilder.newClient().property("CONNECT_TIMEOUT", 10000).property("READ_TIMEOUT", 10000).target(url).request().async().get(String.class), Schedulers
+		            .newThread())
+		            .subscribe(
+		                    next -> System.out.println(next),
+		                    error -> System.err.println(error),
+		                    () -> System.out.println("Stream ended.")
+		            );*/
+			/*Element article = doc.select("article").first();
+			Elements pngs = article.select("[src]");
+
+			String urlHighQuality = pngs.get(1).attr("abs:src");
+			imageUrl.add(urlHighQuality);
+			System.out.println(urlHighQuality);*/
+			URL urls = new URL(url);
+			URLConnection uc = urls.openConnection();
+	        uc.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0");
+	        InputStream inputStream = uc.getInputStream();
+	        
+	        String docString = IOUtils.toString(inputStream, "utf-8");
+	        
+	        Document doc = Jsoup.parse(docString, "");
+			Elements pngs = doc.select("meta");
+			System.out.println(pngs);
+	        /*FileOutputStream  outputStream = new FileOutputStream ("currentImagePage.html");
+
+	        int bytesRead = -1;
+	        int BUFFER_SIZE = 4096;
+
+	        byte[] buffer = new byte[BUFFER_SIZE];
+	        while ((bytesRead = inputStream.read(buffer)) != -1) {
+	            outputStream.write(buffer, 0, bytesRead);
+	        }
+
+	        outputStream.close();
+	        inputStream.close();*/
+			
+		}
+
+		return imageUrl;
 	}
 }
